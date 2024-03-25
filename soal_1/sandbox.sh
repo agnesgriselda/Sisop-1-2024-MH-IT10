a4b7ead04675689836482022b487625a57d23d89
araapk <150429708+araapk@users.noreply.github.com>
2024-03-25 22:09:26 +0700

    Update sandbox.sh

--- soal_1/sandbox.sh
+++ soal_1/sandbox.sh
@@ -7,5 +7,4 @@ cat Sandbox.csv
 awk -F ',' '{print $6, $17}' Sandbox.csv | sort | head -n 1 | awk '{print "Pembeli dengan total sales tertinggi :", $1, $2}'
 awk -F ',' '{print $7, $20}' Sandbox.csv | sort | head -n 1 | awk '{print "Customer segment dengan profit terkecil :", $1, $2}'
 awk -F ',' '{print $14, $20}' Sandbox.csv | sort -nr | head -n 3 | awk '{print "3 kategori yang memiliki profit terkecil :", $1, $2}'
-grep "Adriaens" Sandbox.csv | awk -F ',' '{print $2, $18, $6}' | awk '{print "Purchase date :", $1, $2}, | awk '{print "dan Quantity :", $1, $2}' | awk '{print "dari Nama Adriaens :", $1, $2}'
 grep "Adriaens" Sandbox.csv | awk -F ',' '{print "Purchase date :", $2,"dan Quantity :", $18, "dari Nama Adriaens :", $6}'
